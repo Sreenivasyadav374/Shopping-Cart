07dd9c38ef4a593e8a497dd85b3c4b44f6826bc3
Sreenivasyadav374 <Srinivaspa374@gmail.com>
2023-05-15 14:21:04 +0530

    Routing
    Add Routes to pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Store" element={<Store />}></Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
