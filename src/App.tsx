ef4892d2157ae8f9ac43c180b78c490066470767
Sreenivasyadav374 <Srinivaspa374@gmail.com>
2023-05-15 17:08:43 +0530

    Add Navbar
    Navigation Bar functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <Container>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/About" element={<About />}></Route>
